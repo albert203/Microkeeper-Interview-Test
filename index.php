16841be682390ac17fc4dd92c23fdb3a386a5f44
albert203 <albert.jordaan@outlook.com>
2024-05-08 12:41:36 +1200

    added hashing to the inital user creation

--- index.php
+++ index.php
@@ -100,25 +100,46 @@
         $queryUserOne= "INSERT INTO users (email, password) VALUES ('johndoe@gmail.com', 'john1234')";
         $queryUserTwo= "INSERT INTO users (email, password) VALUES ('testuser@gmail.com', 'test1234')";
         $queryUserThree= "INSERT INTO users (email, password) VALUES ('Janedoe@gmail.com', 'Jane1234')";
-
-        $users = [$queryUserOne, $queryUserTwo, $queryUserThree];
-
         
-        // need to also verify the user is not already in the database 
-        // to prevent duplication
+        $users = [$queryUserOne, $queryUserTwo, $queryUserThree];
+        
+        // hash the passwords
+        function hashPassword($password, $user){
+            $hashedpassword = password_hash($password, PASSWORD_BCRYPT, ['cost' => 12]);
+            return $hashedpassword;
+        }
+        
+
 
         // loop through the users array and insert the data into the database
         // verify the user is not already in the database
         foreach ($users as $user) {
+            // check if the email is already in the database
             $email = explode("'", $user)[1];
             $checkQuery = "SELECT * FROM users WHERE email = '$email'";
             $checkResult = $dbo->prepare($checkQuery);
             $checkResult->execute();
-          
+
+            
+            // If the email is not in the database, hash the password and insert the user
             if ($checkResult->rowCount() == 0) {
-              try {
-                $userPrepared = $dbo->prepare($user);
-                $userPrepared->execute();
+              try {   
+                // explode the password from the user (not in db)
+                $password = explode("'", $user)[3];
+                echo $password;
+                echo '<br>';
+
+                // Hash the password
+                $hashedpassword = hashPassword($password, $user);
+                echo $hashedpassword;
+
+                // Bind the parameters
+                $sql = "INSERT INTO users (email, password) VALUES (:email, :password)";
+                $stmt = $dbo->prepare($sql);
+                $stmt-> bindParam(':email', $email);
+                $stmt-> bindParam(':password', $hashedpassword);
+                $stmt -> execute();
+
                 echo '<div>';
                 echo '<p>' . $email . ' Created Successfully</p>';
                 echo '</div>';
@@ -133,79 +154,88 @@
             }
           }
         
-        // // gets the form data
-        // if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-        //     $email = $_POST['email'];
-        //     $password = $_POST['password'];
-        //     echo $email;
-        //     echo $password;
+        // gets the form data
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $email = $_POST['email'];
+            $password = $_POST['password'];
+            echo $email;
+            echo $password;
             
+            // LOGIN 
+
+            // query to select the user email
+            $queryEmail = "SELECT * users WHERE ($email) = (:email)";
+            $queryPassword = "SELECT * users WHERE ($password) = (:password)";
+
+            $stmt = $dbo->prepare($query);
+
+            try {
+                $duplicateEmail = $stmt -> execute();
+            } catch (PDOException $e) {
+                header('Location: ../index.php');
+                echo 'Email does not exist';
+            }
+
+
+
+
             
 
-        //     // Create User, Insert the data into the database
+           
 
-        //     $query = "INSERT INTO  users ($email, $password) VALUES (:email, :password)";
-        //     $stmt = $dbo->prepare($query);
+            //ERROR HANDLING
+            $errors = [];
 
-        //     // hash the password
-        //     $hashedpassword = password_hash($password, PASSWORD_BCRYPT, ['cost' => 12]);
-        //     $stmt -> bindParam(':email', $email);
-        //     $stmt -> bindParam(':password', $hashedpassword);
-        //     $stmt -> execute();
+            // I email is missing 
+            if (is_input_empty($email, $password)) {
+                $errors["empty_input"] = 'Email is required';
+            }
 
-        //     //ERROR HANDLING
-        //     $errors = [];
+            // If email is invalid
+            if (is_email_valid($email)){
+                $errors["invalid_email"] = 'Email is invalid';
+            }
 
-        //     // I email is missing 
-        //     if (is_input_empty($email, $password)) {
-        //         $errors["empty_input"] = 'Email is required';
-        //     }
-
-        //     // If email is invalid
-        //     if (is_email_valid($email)){
-        //         $errors["invalid_email"] = 'Email is invalid';
-        //     }
-
-        //     if ($errors){
-        //         echo $_SESSION['errors'] = $errors;
-        //         header('Location: ../index.php');
-        //         die();
-        //     }
+            if ($errors){
+                echo $_SESSION['errors'] = $errors;
+                header('Location: ../index.php');
+                die();
+            }
 
 
-        //     // PasswordVerification($password, $hashedpassword);
+            // PasswordVerification($password, $hashedpassword);
 
-        // // } else {
-        // //     // header('Location: ../index.php');
-        // //    echo "No data was sent";
-        // // }
+        } else {
+            // header('Location: ../index.php');
+           echo "No data was sent";
+        }
 
-        // // When Login button is clicked, verify the password
-        // function PasswordVerification($password, $hashedpassword){
-        //     // verify hashed password is correct, compares to database
-        //     $password = $_POST['password'];
-        //     if (password_verify($password, $hashedpassword)) {
-        //         echo 'Password is correct';
-        //     } else {
-        //         echo 'Password is incorrect';
-        //     }
-        // }
+        // When Login button is clicked, verify the password
+        function PasswordVerification($password, $hashedpassword){
+            // verify hashed password is correct, compares to database
+            $password = $_POST['password'];
+            if (password_verify($password, $hashedpassword)) {
+                echo 'Password is correct';
+            } else {
+                echo 'Password is incorrect';
+            }
+        }
 
-        // function is_input_empty($email, $password){
-        //     if (empty($email) || empty($password)) {
-        //         return true;
-        //     } else {
-        //         return false;
-        //     }
-        // }
+        function is_input_empty($email, $password){
+            if (empty($email) || empty($password)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
 
-        // function is_email_valid($email){
-        //     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        //         return true;
-        //     } else {
-        //         return false;
-        //     }
-        // }
+        function is_email_valid($email){
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                return true;
+            } else {
+                return false;
+            }
+        }
 
 ?>
 
